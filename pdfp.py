aef118f1e33575c4d9a4d17e762abe1f18f401e9
Tse <michytsemichy@gmail.com>
2018-07-18 16:43:26 -0700

    Added further instructions

--- pdfp.py
+++ pdfp.py
@@ -5,5 +5,15 @@ the need to convert the pdf file to a text file beforehand, therefore
 saving time. We will be using the Python PyPDF2 module to accomplish
 this task. 
 
+Installation: 
+
+pip install PyPDF2
+
+If there is some authentication and/or proxy error, try:
+
+sudo -E pip install PyPDF2
+
 """
+
+import PyPDF    # This is the module we need
 
