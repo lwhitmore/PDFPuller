38f1793d2a486a1b14adac905eb1567c3e604beb
Tse <michytsemichy@gmail.com>
2018-07-18 16:29:23 -0700

    Added the data extracting file

---
+++ pdfp.py
@@ -0,0 +1,9 @@
+"""
+
+The purpose of this program is to extract data from a pdf file without 
+the need to convert the pdf file to a text file beforehand, therefore
+saving time. We will be using the Python PyPDF2 module to accomplish
+this task. 
+
+"""
+
