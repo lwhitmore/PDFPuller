2f2324c2098dacaef046ec9ee6d8cb15bf1e455a
Tse <michytsemichy@gmail.com>
2018-07-18 17:17:22 -0700

    Added code

--- pdfp.py
+++ pdfp.py
@@ -15,5 +15,17 @@ sudo -E pip install PyPDF2
 
 """
 
-import PyPDF    # This is the module we need
+import PyPDF2    # This is the module we need
+
+"We need to read the pdf first, then extract text from the target page"
+
+# Open the file and create a pdf reader object
+pdf_file = open('2007-1.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(pdf_file) 
+
+# print(pdf_reader.numPages) # Print to test if the # of pages is correct
+
+# Get a particular page from the reader object and extract the text
+
+
 
