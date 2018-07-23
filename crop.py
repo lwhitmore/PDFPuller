fe5bf16352f414ba8f95bfe1cf9c5f0663057cd6
Tse <michytsemichy@gmail.com>
2018-07-23 14:59:11 -0700

    Experiment with cropping pages

---
+++ crop.py
@@ -0,0 +1,11 @@
+"""
+
+Purpose: This file is to experiment with cropping pages of a pdf because 
+in most cases, we are not going to want to extract data from an entire 
+page, but rather, from one portion of the page (e.g. a table on the page). 
+
+To accomplish this, we will be experimenting with the cropBox function 
+of PyPDF2. 
+
+"""
+
