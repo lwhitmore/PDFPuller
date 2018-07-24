089e45147e5535b204b762274167af8bab29e8ac
Tse <michytsemichy@gmail.com>
2018-07-24 10:21:04 -0700

    Added new files pdfcat and table for table extracting

---
+++ pdfcat.py
@@ -0,0 +1,7 @@
+"""
+
+This script is taken directly from the PyPDF2 GitHub page:
+https://github.com/mstamy2/PyPDF2/blob/master/Scripts/pdfcat. 
+
+"""
+
---
+++ table.py
@@ -0,0 +1,17 @@
+"""
+
+Purpose:
+
+The goal of table.py is to use pdfcat (a script from PyPDF2) and tabula-py
+in order to select the pages we want from the original pdf to create a pdf
+that is smaller and contains only the pages with the tables in it, such that
+we can use tabula-py to extract the tables from the smaller pdf. 
+
+Note:
+
+You must know the page number(s) to input. 
+
+With pdfcat, the script should be able to take in multiple pdf inputs to 
+create one smaller pdf. 
+
+"""
